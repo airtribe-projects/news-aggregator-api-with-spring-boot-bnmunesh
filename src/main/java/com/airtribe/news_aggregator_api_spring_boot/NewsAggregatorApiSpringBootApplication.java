5f31b3a8536399eaa0087e48e0da629969cde48e
bnmunesh <muneshkumarbn020@gmail.com>
2024-08-18 19:35:13 +0530

    initial-commit

---
+++ src/main/java/com/airtribe/news_aggregator_api_spring_boot/NewsAggregatorApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.news_aggregator_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorApiSpringBootApplication.class, args);
+	}
+
+}
